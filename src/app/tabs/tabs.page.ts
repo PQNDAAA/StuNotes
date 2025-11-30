3e021acdd340777fa6154d26d08912ec2461044b
PQNDAAA <gayvallet.dylan@gmail.com>
2025-11-30 20:00:52 +0100

    Changing Home Page Design & Fixing Bugs on the header scroll

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ModalController} from "@ionic/angular";
+import {AddnoteComponent} from "../cards/addnote/addnote.component";
 
 @Component({
   selector: 'app-tabs',
@@ -8,6 +10,16 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private mc : ModalController) {}
+
+  async openPopup(){
+    const modal = await this.mc.create({
+      component: AddnoteComponent
+    });
+
+    await modal.present();
+  }
+
 
 }
+
